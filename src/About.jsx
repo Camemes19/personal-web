ee17b519207bfd1d0da65621dff24c6f8dbbdb64
Camemes19 <jamiesoncameron19@gmail.com>
2025-12-04 18:37:27 -0800

    feat: series of basic css changes, added placeholder pfp for the time.

--- src/About.jsx
+++ src/About.jsx
@@ -1,27 +1,29 @@
 import {LuUser} from "react-icons/lu";
 
-function About() {
+export function About() {
 
 
     return (
         <div>
-            <h1>
-                <LuUser/>
+            <h2>
+                <LuUser className={"heading-icon"}/>
                 Profile
-            </h1>
-            <p>
+            </h2>
+            <img className={"profile-photo"} src={"images/quattro.webp"} alt={"Image of Quattro Bajeena from Mobile Suit Zeta" +
+                " Gundam"}/>
+            <p className={"profile"}>
                 Hey, I'm Cameron Jamieson, a junior software developer with over 2.5 years of experience based out of
                 Seattle, Washington. I've been specializing in full-stack development with back-ends built
                 on <b>AWS</b> using <b>Java</b> and front-ends powered by <b>NodeJS</b> and <b>React</b>.
             </p>
-            <p>
+            <p className={"profile"}>
                 As part of this, I have extensive experience in Java and JavaScript testing frameworks
                 like <b>JUnit</b>, <b>Jest</b>, and <b>Cypress</b> for front-end test automation. I'm passionate about
                 optimizing the everyday operations of projects I work on, making sure users have high-quality, reliable
                 experiences and developers have robust, low-maintenance services to avoid the ever-present enemy of
                 developer evenings: a late-night page.
             </p>
-            <p>
+            <p className={"profile"}>
                 When I'm not working on projects, I love building Gunpla, playing card games, organizing esports
                 tournaments for my local fighting game scene, and relaxing at home with my cat, Emmy.
             </p>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,30 +1,32 @@
 import { useState } from 'react';
-import {LuGithub, LuLinkedin} from "react-icons/lu";
+import { LuGithub, LuLinkedin} from "react-icons/lu";
 
 function Footer() {
     const [date] = useState(() => new Date());
 
     return (
         <footer>
-            <p>
+            <div className={"copyright"}>
                 © Cameron Jamieson {date.getFullYear()}
-            </p>
-            <a
-                href={"https://github.com/Camemes19"}
-                target={"_blank"}
-                aria-label={"GitHub Repo"}
-                rel={"noopener noreferrer"}
-            >
-                <LuGithub />
-            </a>
-            <a
-                href={"https://www.linkedin.com/in/cameronjamieson19"}
-                target={"_blank"}
-                aria-label={"Linkedin Profile"}
-                rel={"noopener noreferrer"}
-            >
-                <LuLinkedin />
-            </a>
+            </div>
+            <div className={"socials"}>
+                <a
+                    href={"https://github.com/Camemes19"}
+                    target={"_blank"}
+                    aria-label={"GitHub Repo"}
+                    rel={"noopener noreferrer"}
+                >
+                    <LuGithub />
+                </a>
+                <a
+                    href={"https://www.linkedin.com/in/cameronjamieson19"}
+                    target={"_blank"}
+                    aria-label={"Linkedin Profile"}
+                    rel={"noopener noreferrer"}
+                >
+                    <LuLinkedin />
+                </a>
+            </div>
         </footer>
     )
 }
