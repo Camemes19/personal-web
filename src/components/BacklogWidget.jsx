865980b2189b2cdd260b744ed1a216a04dacf709
Camemes19 <jamiesoncameron19@gmail.com>
2026-08-06 12:08:52 -0700

    Feat: Updated BacklogWidget to support updating percentages

--- src/components/BacklogWidget.jsx
+++ src/components/BacklogWidget.jsx
@@ -1,16 +1,23 @@
 import Indicator from "./Indicator.jsx";
 
 
-function BacklogWidget() {
+function BacklogWidget({complete, inProgress, unfinished}) {
+    const totalKits = complete+inProgress+unfinished;
+    const completePercent = complete/totalKits;
+    const inProgressPercent = inProgress/totalKits;
+    const unfinishedPercent = unfinished/totalKits;
+
     return (
         <div className={"mx-auto max-w-2/5 border-2 rounded-lg border-indigo-900 shadow-lg shadow-gray-950/30 p-2"}>
             <h4 className={"text-left m-2"}>Backlog Tracker</h4>
             <div className={"m-3 h-4 max-w-9/10 bg-gray-500 flex -skew-x-24 rounded-br-md rounded-tl-md"}>
-                <div style={{width: "50%"}} className={"bg-red-500 flex rounded-tl-md drop-shadow-md" +
+                <div style={{width: `calc(100%*${unfinishedPercent}`}} className={"bg-red-500 flex rounded-tl-md" +
+                    " drop-shadow-md" +
                     " drop-shadow-red-700"}></div>
-                <div style={{width: "5%"}} className={"bg-yellow-500 flex drop-shadow-md" +
+                <div style={{width: `calc(100%*${inProgressPercent}`}} className={"bg-yellow-500 flex drop-shadow-md" +
                     " drop-shadow-yellow-700"}></div>
-                <div style={{width: "45%"}} className={"bg-green-600 flex rounded-br-md drop-shadow-md" +
+                <div style={{width: `calc(100%*${completePercent}`}} className={"bg-green-600 flex rounded-br-md" +
+                    " drop-shadow-md" +
                     " drop-shadow-green-800"}></div>
             </div>
             <div>
